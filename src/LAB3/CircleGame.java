9e22cc394902c2906e7b96b9e2e65a91523555d3
Hazuan-Surface <223877@student.upm.edu.my>
2024-11-26 10:43:39 +0800

    update

--- src/LAB3/CircleGame.java
+++ src/LAB3/CircleGame.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -20,6 +21,8 @@ import java.util.Random;
 public class CircleGame extends Application {
     public static final int RADIUS = 15;
     public int clickCounter = 0;
+    Random randomNumberGenerator = new Random();
+
 
     public static void main(String[] args) {
         launch(args);
@@ -27,20 +30,24 @@ public class CircleGame extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        //Creating main VBox pane
         VBox mainPane = new VBox();
         mainPane.setAlignment(Pos.CENTER);
         mainPane.setSpacing(20);
         mainPane.setPadding(new Insets(15));
         mainPane.setStyle("-fx-background-color: #ADD8E6;");
 
-        Pane circlePane = new Pane();
-        circlePane.setMinHeight(300);
+        //Creating pane for circle game
+        BorderPane circlePane = new BorderPane();
+        circlePane.setPrefSize(400,300);
         circlePane.setStyle("-fx-background-color: #FFFFFF;");
 
+        //Setting the titlw of the game
         Label timeAndTitle = new Label("! Circle Game !");
         timeAndTitle.setFont(Font.font("Impact", FontWeight.BOLD, 30));
         mainPane.getChildren().add(timeAndTitle);
 
+        //Image to popup after finished
         Image image = new Image("file:\\C:\\Users\\User\\IdeaProjects\\CSC3104 - Advanced Programming Lab\\src\\LAB3\\minion-congrats.gif");
         ImageView congratsGif = new ImageView(image);
         congratsGif.setFitWidth(350);
@@ -48,10 +55,16 @@ public class CircleGame extends Application {
 
         mainPane.getChildren().add(circlePane);
 
-        Circle circle = new Circle(70, 70, RADIUS);
+        //Creating the first circle to be at random position and color
+        double firstXLocation = randomNumberGenerator.nextDouble() * (400 - 2 * RADIUS);
+        double firstYLocation = randomNumberGenerator.nextDouble() * (300 - 2 * RADIUS);
+        Circle circle = new Circle(firstXLocation, firstYLocation, RADIUS);
+        circle.setFill(Color.color(randomNumberGenerator.nextDouble(), randomNumberGenerator.nextDouble(), randomNumberGenerator.nextDouble()));
         circlePane.getChildren().add(circle);
 
+        //start the timer
         long startTime = System.currentTimeMillis();
+        //On mouse click:
         circle.setOnMouseClicked(e -> {
             circlePane.getChildren().clear();
             clickCounter++;
@@ -60,24 +73,27 @@ public class CircleGame extends Application {
                 newCircle.setOnMouseClicked(circle.getOnMouseClicked());
                 circlePane.getChildren().add(newCircle);
             } else {
+                //Stop the timer
                 long endTime = System.currentTimeMillis();
                 long timeSpent = endTime - startTime;
                 timeAndTitle.setFont(Font.font("Segoe UI", FontWeight.BLACK, 25));
                 timeAndTitle.setText("Time spent: " + timeSpent + " milliseconds");
                 mainPane.getChildren().remove(circlePane);
                 mainPane.getChildren().add(congratsGif);
+                //Expanding the stage
+                primaryStage.setWidth(800);
             }
         });
 
         Scene scene = new Scene(mainPane, 400, 400);
-        primaryStage.setTitle("Exercise15_19");
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Exercise15_19");
         primaryStage.show();
     }
 
     //Method to create new circle
     public Circle createCircle(Pane circlePane) {
-        Random randomNumberGenerator = new Random();
+        //Creating circle at random position and color
         double locationX = randomNumberGenerator.nextDouble() * (circlePane.getWidth() - 2 * RADIUS) + RADIUS;
         double locationY = randomNumberGenerator.nextDouble() * (circlePane.getHeight() - 2 * RADIUS) + RADIUS;
         Circle circle = new Circle(locationX, locationY, RADIUS);
